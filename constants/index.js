a85c1a95d9f5b584838f4598cc2c87f0d3e0b31f
shiaxie <106312669+shiaxie@users.noreply.github.com>
2023-07-30 15:48:40 +0400

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xa7d62353a49821deb82c5ebcc42964b0e56e7200";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
